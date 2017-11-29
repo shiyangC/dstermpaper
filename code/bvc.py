f543b692bf470f88faf4edf702dc80f228d7f7d1
k7sun <kyle.sung@utexas.edu>
2017-11-29 14:30:43 -0600

    Get VG BVC working, plot points. there seems to be some problem with the result as they all lie on the x=y line.

--- code/bvc.py
+++ code/bvc.py
@@ -4,48 +4,48 @@
 #from scipy.spatial import ConvexHull
 #from matplotlib.path import Path
 from shapely.geometry import Point, MultiPoint, Polygon, mapping
-
 from math import sin, cos, pi
-
 import random
 from itertools import combinations
+import matplotlib.pyplot as plt
 
 #p: process, r: round, c: content message
 def RbSend(p, r, v):
     return
 
-def RbReceiveWitness(points, f):
-    badPoint = [-1] * len(points[0])
-    np = points[:]
-    np[:f] = [badPoint for x in range(f)]
-    return np
-    # random.shuffle(np)
+def RbReceiveWitness(points, f): #produces n - f good points and f bad points with random values
+    badPoint = lambda:[random.uniform(-2,2)] * len(points[0])
+    #np = points[:]
+    #np[:-f] = [badPoint() for x in range(f)]
+    badPoints = [badPoint() for x in range(f)]
+    return points + badPoints
+    #random.shuffle(np)
     # return np[:-f]
 
 
 def BVC_MH():
     return
 
-def BVC_VG(points, f):
-    #init procs here
+def BVC_VG(goodPts, f):
     rounds = []
-    rounds.append(points)
+    rounds.append(RbReceiveWitness(goodPts, f))
     R = CalculateVgRound()
     for r in range(R):
-        points = RbReceiveWitness(rounds[r], f)
-        newPts = VG_Round(points, f)
+        newPts = []
+        for p in range(len(goodPts)+f):
+            points = RbReceiveWitness(rounds[r][:-f], f)
+            newPt = VG_Round(points, f)
+            newPts.append(newPt)
         rounds.append(newPts)
     return rounds
 
-# every process will run this. But since every process has the same input because of RbReceive, just one process is enough
 def VG_Round(points, f):
     z = []
     k = len(points) - f
     for subset in combinations(points, k):
         safe = CalculateSafeArea(subset, f)
         z.append(VG_DeterministicallyChoosePoint(safe))
-    v = VG_avgPoints(z) #this part is broken. I don't understand how v will be different for every non-faulty process,
-    # since RBReceiveWitness should guarantee that every process receives the same message?
+    v = VG_avgPoints(z)
     return v
 
 def VG_DeterministicallyChoosePoint(safeArea):
@@ -64,9 +64,9 @@ def VG_avgPoints(points):
     return avg
 
 def CalculateVgRound():
-    return 10
+    return 5
 def CalculateMhRound():
-    return 10
+    return 5
 
 def CalculateSafeArea(points, f):
     k = len(points) - f
@@ -75,12 +75,6 @@ def CalculateSafeArea(points, f):
         subarea = MultiPoint(subset).convex_hull
         area = area.intersection(subarea)
     return area
-
-# def pcontains(points, point):
-#     points = array(points, float)
-#     hull = ConvexHull(points)
-#     hull_path = Path(points[hull.vertices])
-#     return hull_path.contains_point(point)
 
 def pcontains(points, point):
     hull = MultiPoint(points).convex_hull
@@ -93,10 +87,22 @@ print pcontains(points, Point(1,1,1))
 print RbReceiveWitness(points, 1)
 print RbReceiveWitness(points, 2)
 
-points = [(-10, -10)] + [(cos(t), sin(t)) for t in [t * 2 * pi / 5.0 for t in range(5)]]
+points = [[cos(t), sin(t)] for t in [t * 2 * pi / 5.0 for t in range(5)]]
 print points
 print CalculateSafeArea(points, 1)
 
-print BVC_VG(points, 1)
+print 'result'
+bvc_result = BVC_VG(points, 1)
+colors = 'grcmykwgrcmykw'
+for round, col in zip(bvc_result, colors[:len(bvc_result)]):
+    points = [Point(xy) for xy in round]
+    avgPnt = VG_avgPoints(points)
+    xcoords = [xy[0] for xy in round]
+    ycoords = [xy[1] for xy in round]
+    plt.plot(xcoords, ycoords, col+'o')
+    plt.plot([avgPnt[0]],[avgPnt[1]], 'bx')
+    plt.axis([-1, 1, -1, 1])
+
+plt.show()
 
 #POLYGON ((0.309 -0.225, -0.118 -0.363, -0.382 0, -0.118 0.363, 0.309 0.225, 0.309 -0.225))
