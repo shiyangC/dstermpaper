79160603e0dda4e2e874e50d8220d0f4b00b637a
k7sun <sung.kyle@gmail.com>
2017-11-30 09:55:43 -0600

    implement MH algo. errors still

--- code/bvc.py
+++ code/bvc.py
@@ -14,19 +14,68 @@ def RbSend(p, r, v):
     return
 
 def RbReceiveWitness(points, f): #produces n - f good points and f bad points with random values
-    badPoint = lambda:[random.uniform(-2,2)] * len(points[0])
+    badPoint = lambda:tuple([round(random.uniform(-1,1),2) for i in range(len(points[0]))])
     #np = points[:]
     #np[:-f] = [badPoint() for x in range(f)]
     badPoints = [badPoint() for x in range(f)]
+    print "badPoints" , badPoints
+#   for bp in badPoints:
+#        plt.plot(bp, 'rx')
     return points + badPoints
     #random.shuffle(np)
     # return np[:-f]
 
 
-def BVC_MH():
-    return
+def BVC_MH(goodPts, f):
+    plotPoly(Polygon(goodPts), color='black', linewidth=2)
+    plotPoly(CalculateSafeArea(goodPts,1), color='blue', linewidth=2)
+    colors = 'rcygbkp'
+
+    R = CalculateMhRound()
+
+    n = len(goodPts) + f
+    rounds = [[(0, 0)] * n] * (R + 1)
+    rounds[0] = RbReceiveWitness(goodPts, f)
+
+    d = len(goodPts[0])
+    for m in range(d):
+        for rIdx in range(1,R+1):
+            H = [0] * n #halt msg received in each process
+            r = [1] * n #round in each process
+            if rIdx == 2:
+                a=1+1
+            for p in range(n):
+                #while H[p] <= f:  #assume H received immedately
+                    #RbSend()
+                    points = RbReceiveWitness(rounds[rIdx-1][:-f],f)
+                    safe = CalculateSafeArea(points, f)
+                    v = MH_DeterministicallyChoosePoint(safe, m)
+                    rounds[rIdx][p] = v
+                    if r[p] == R:
+                        H = [c+1 for c in H]
+                    r[p] = r[p]+1
+
+                #RbReceive
+                #for p in range(n):
+                    # can deduct some of H to simulate delayed received halt msgs?
+
+    for round in rounds:
+        plotPoly(ptsToHull(round), color=colors[r], linewidth=1)
+    return rounds
+
+def MH_DeterministicallyChoosePoint(safeAreaPgon, dim):
+    if safeAreaPgon.geom_type == "LineString":
+        x,y = safeAreaPgon.centroid.xy
+        return [x,y]
+    points = polyToPts(safeAreaPgon)
+    points.sort(key=lambda p:p[dim])
+    midpoint = points[len(points)/2]
+    return midpoint
 
 def BVC_VG(goodPts, f):
+    plotPoly(Polygon(goodPts), color='black', linewidth=2)
+    plotPoly(CalculateSafeArea(goodPts,1), color='blue', linewidth=2)
+    colors = 'rcygbkp'
     rounds = []
     rounds.append(RbReceiveWitness(goodPts, f))
     R = CalculateVgRound()
@@ -37,6 +86,7 @@ def BVC_VG(goodPts, f):
             newPt = VG_Round(points, f)
             newPts.append(newPt)
         rounds.append(newPts)
+        plotPoly(ptsToHull(newPts), color=colors[r], linewidth=1)
     return rounds
 
 def VG_Round(points, f):
@@ -72,6 +122,7 @@ def CalculateSafeArea(points, f):
     k = len(points) - f
     area = MultiPoint(points).convex_hull
     for subset in combinations(points, k):
+        print subset
         subarea = MultiPoint(subset).convex_hull
         area = area.intersection(subarea)
     return area
@@ -80,29 +131,66 @@ def pcontains(points, point):
     hull = MultiPoint(points).convex_hull
     return hull.contains(point)
 
-points = [(0,0,0),(1,0,0),(0,1,0), (0,0,1)]
-print pcontains(points,Point(0.1,0.1,0.1))
-print pcontains(points, Point(0,0,0))
-print pcontains(points, Point(1,1,1))
-print RbReceiveWitness(points, 1)
-print RbReceiveWitness(points, 2)
+def ptsToPoly(points):
+    return Polygon(points)
+def polyToPts(poly):
+    try:
+        x,y = poly.exterior.xy
+    except:
+        a = 1+1
+    l = zip(x,y)
+    q = set()
+    r = []
+    for t in l:
+        if t not in q:
+            q.add(t)
+            r.append(t)
+    return r
+def ptsToHull(points):
+    return MultiPoint(points).convex_hull
+def polyToHull(poly):
+    return ptsToHull(ptsToPoly(poly))
+def hullToPts(hull):
+    return polyToPts(hull)
+def hullToPoly(hull):
+    return Polygon(hull)
 
-points = [[cos(t), sin(t)] for t in [t * 2 * pi / 5.0 for t in range(5)]]
-print points
-print CalculateSafeArea(points, 1)
+def plotPoly(poly,name='fig 1', color='#6699cc', alpha=0.7, linewidth=1, zorder=2):
+    x,y = poly.exterior.xy
+    plt.plot(x,y,color=color, alpha=alpha, linewidth=linewidth, zorder=zorder)
+    plt.title(name)
+#    plt.show()
+    return
 
-print 'result'
-bvc_result = BVC_VG(points, 1)
-colors = 'grcmykwgrcmykw'
-for round, col in zip(bvc_result, colors[:len(bvc_result)]):
-    points = [Point(xy) for xy in round]
-    avgPnt = VG_avgPoints(points)
-    xcoords = [xy[0] for xy in round]
-    ycoords = [xy[1] for xy in round]
-    plt.plot(xcoords, ycoords, col+'o')
-    plt.plot([avgPnt[0]],[avgPnt[1]], 'bx')
-    plt.axis([-1, 1, -1, 1])
+def plotPolyXY(points, name='fig 1', color='#6699cc', alpha=0.7, linewidth=1, zorder=2):
+    plotPoly(Polygon(points), name=name, color=color, alpha=alpha, linewidth=linewidth, zorder=zorder)
 
-plt.show()
+def plotPoints(points, name='fig 1', color='#6699cc', alpha=0.7, linewidth=1, zorder=2):
+    x = [xy[0] for xy in points]
+    y = [xy[1] for xy in points]
+    plt.plot(x,y,color=color, alpha=alpha, linewidth=linewidth, zorder=zorder)
+    plt.title(name)
+#    plt.show()
+    return
+
+if __name__ == "__main__":
+    # points = [(0,0,0),(1,0,0),(0,1,0), (0,0,1)]
+    # print pcontains(points,Point(0.1,0.1,0.1))
+    # print pcontains(points, Point(0,0,0))
+    # print pcontains(points, Point(1,1,1))
+    # print RbReceiveWitness(points, 1)
+    # print RbReceiveWitness(points, 2)
+
+    points = [[round(cos(t),2), round(sin(t),2)] for t in [t * 2 * pi / 5.0 for t in range(5)]]
+#    print points
+#    print CalculateSafeArea(points, 1)
+
+    print 'result'
+#    bvc_result = BVC_VG(points, 1)
+    bvc_result = BVC_MH(points, 1)
+
+    plt.axes().set_aspect(1)
+    plt.show()
+
 
 #POLYGON ((0.309 -0.225, -0.118 -0.363, -0.382 0, -0.118 0.363, 0.309 0.225, 0.309 -0.225))
